b02fb5132ca1d21928b27e4abacc9761110f9ebe
Milan Bloem <milanbloem@gmail.com>
2022-11-28 17:53:01 +0100

    Prepping transition from example to yamb scores setup

--- djangorest/api/models.py
+++ djangorest/api/models.py
@@ -3,8 +3,10 @@ from django.db import models
 class Bucketlist(models.Model):
     """This class represents the bucketlist model."""
     name = models.CharField(max_length=255, blank=False, unique=True)
-    date_created = models.DateTimeField(auto_now_add=True)
-    date_modified = models.DateTimeField(auto_now=True)
+    # player1_name = models.CharField(max_length=255, blank=False)
+    # player1_score = models.SmallIntegerField()
+    # player2_name = models.CharField(max_length=255, blank=False)
+    # player2_score = models.SmallIntegerField()
 
     def __str__(self):
         """Return a human readable representation of the model instance."""
